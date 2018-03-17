ba747e1b66ee10c35424a45bc825e91d9fb11ff9
Rzeszow <mdhm@wp.pl>
2018-03-17 20:09:11 +0100

    Poprawienie stylu.

--- SteamKeyGenerator.php
+++ SteamKeyGenerator.php
@@ -1,41 +1,36 @@
 <?php
-$GLOBALS['keys'] = array();
 function gen() {
-        $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $charactersLength = strlen($characters);
-        $randomString1 = '';
-        $randomString2 = '';
-        $randomString3 = '';
-        for ($i = 0; $i < 5; $i++) {
-                $randomString1 .= $characters[rand(0, $charactersLength - 1)];
-        }
-        for ($i = 0; $i < 5; $i++) {
-                $randomString2 .= $characters[rand(0, $charactersLength - 1)];
-        }
-        for ($i = 0; $i < 5; $i++) {
-                $randomString3 .= $characters[rand(0, $charactersLength - 1)];
-        }
-        $key = $randomString1 . "-" . $randomString2 . "-" . $randomString3;
-        if(!in_array($key, $GLOBALS['keys'])) {
-                $GLOBALS['keys'][] .= $key;
-                return $key;
-        } else {
-                echo "Key already generated.";
-                gen();
-        }
+    $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString1 = '';
+    $randomString2 = '';
+    $randomString3 = '';
+    for ($i = 0; $i < 5; $i++) {
+            $randomString1 .= $characters[rand(0, $charactersLength - 1)];
+    }
+    for ($i = 0; $i < 5; $i++) {
+            $randomString2 .= $characters[rand(0, $charactersLength - 1)];
+    }
+    for ($i = 0; $i < 5; $i++) {
+            $randomString3 .= $characters[rand(0, $charactersLength - 1)];
+    }
+    $key = $randomString1 . "-" . $randomString2 . "-" . $randomString3;
+
+    echo $key . PHP_EOL;
+    return $key;
 }
 
-$shortopts .= "a:";  // Required value (-a)
-$longopts  = array(
-    "amount:",
-);
-$options = getopt($shortopts, $longopts);
+$options = getopt("a:"); // Required value (-a)
 
-$save = array();
-$i = $options['a'];
-while($i > 0) {
-		$save[] = gen();
-		$i = $i-1;
+$a = $options['a'];
+if (is_numeric($a) && $a > 0) {
+    while ($a > 0) {
+        $save[] = gen();
+        $a--;
+    }
+} else {
+    echo "Enter the amount.";
 }
+
 $saveFile = "keys.txt";
 file_put_contents($saveFile, implode("\n", $save));
